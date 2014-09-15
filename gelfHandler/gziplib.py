257a3ddf880bdb5351ed5915248af37f2998b0f9
Andrew Robbins <and_j_rob@yahoo.com>
2014-09-15 17:25:10 -0400

    Removed null

---
+++ gelfHandler/gziplib.py
@@ -0,0 +1,33 @@
+import gzip
+import tempfile
+
+def compress(string, level=9, bufsize=-1):
+    compressedtext = None
+    tempfilename = None
+
+    with tempfile.NamedTemporaryFile("wr", bufsize, "gziplib") as ntf:
+        tempfilename = ntf.name
+
+        with gzip.GzipFile(ntf.name, "w", level) as gzf:
+            gzf.write(string)
+
+        with open(ntf.name, "r") as gzf:
+            compressedtext = gzf.read()
+
+    return compressedtext
+    
+def decompress(string, bufsize=-1):
+    plaintext = None
+    tempfilename = None
+
+    with tempfile.NamedTemporaryFile("wr", bufsize, "gziplib") as ntf:
+        tempfilename = ntf.name
+
+        with open(ntf.name, "w") as gzf:
+            gzf.write(string)
+
+        with gzip.GzipFile(ntf.name, "r") as gzf:
+            plaintext = gzf.read()
+
+    return plaintext
+
