8d2b96d265b34e3cb477c7cfd2e987b351a3964c
Andrew Robbins <and_j_rob@yahoo.com>
2017-06-23 11:30:17 -0400

    Fixed unboundlocalerror

--- gelfHandler/gelf.py
+++ gelfHandler/gelf.py
@@ -11,8 +11,14 @@ from .transport import ThreadedTCPTransport, ThreadedUDPTransport
 
 
 class gelfHandler(logging.Handler):
+    '''
+    TODO
+    '''
 
     def __init__(self, **kw):
+        '''
+        TODO
+        '''
         self.proto = kw.get('proto', 'UDP')
         self.host = kw.get('host', 'localhost')
         self.port = kw.get('port', None)
@@ -21,6 +27,7 @@ class gelfHandler(logging.Handler):
         self.fromHost = kw.get('fromHost', socket.getfqdn())
         self.globalProps = kw.get('gelfProps', {})
         self.tls = kw.get('tls', False)
+        self._transport = None
         if self.proto == 'UDP':
             self.connectUDPSocket()
         if self.proto == 'TCP':
@@ -28,14 +35,23 @@ class gelfHandler(logging.Handler):
         logging.Handler.__init__(self)
 
     def connectUDPSocket(self):
+        '''
+        TODO
+        '''
         url = 'udp://%s:%d/' % (self.host, int(self.port or 12202))
         self._transport = ThreadedUDPTransport(url)
 
     def connectTCPSocket(self):
+        '''
+        TODO
+        '''
         url = 'tcp://%s:%d/' % (self.host, int(self.port or 12201))
         self._transport = ThreadedTCPTransport(url)
 
-    def buildMessage(self, record, **kwargs):
+    def buildMessage(self, record, **_):
+        '''
+        TODO
+        '''
         recordDict = record.__dict__
         msgDict = {}
         msgDict['version'] = '1.1'
@@ -59,46 +75,48 @@ class gelfHandler(logging.Handler):
             msgDict['facility'] = recordDict['name']
         extra_props = recordDict.get('gelfProps', None)
         if isinstance(extra_props, dict):
-            for k, v in extra_props.iteritems():
-                msgDict['_' + k] = v
+            for key, value in extra_props.iteritems():
+                msgDict['_' + key] = value
         global_props = self.globalProps
         if isinstance(global_props, dict):
-            for k, v in global_props.iteritems():
-                msgDict['_' + k] = v
+            for key, value in global_props.iteritems():
+                msgDict['_' + key] = value
         return msgDict
 
     def dumps(self, data):
+        '''
+        TODO
+        '''
         return json.dumps(data)
 
     def formatMessage(self, msgDict):
+        '''
+        TODO
+        '''
         if self.proto == 'UDP':
             msg = self.dumps(msgDict)
         if self.proto == 'TCP':
             msg = self.dumps(msgDict) + '\0'
         return msg
 
-    def sendOverTCP(self, msg):
-        totalsent = 0
-        while totalsent < len(msg):
-            sent = self.sock.send(msg[totalsent:])
-            if sent == 0:
-                raise IOError("socket connection broken")
-            totalsent = totalsent + sent
-
-    def emit(self, record, **kwargs):
+    def emit(self, record, **_):
+        '''
+        TODO
+        '''
+        msgDict = None
         try:
             self.format(record)
-            msgDict = self.buildMessage(record, **kwargs)
+            msgDict = self.buildMessage(record)
             msg = self.formatMessage(msgDict)
-        except UnicodeEncodeError as e:
-            e.data = msgDict
-            self.emit_failure(e, level=logging.WARNING)
-        except ValueError as e:
-            e.data = msgDict
-            self.emit_failure(e)
-        except Exception as e:
-            e.data = msgDict
-            self.emit_failure(e)
+        except UnicodeEncodeError as err:
+            err.data = msgDict
+            self.emit_failure(err, level=logging.WARNING)
+        except ValueError as err:
+            err.data = msgDict
+            self.emit_failure(err)
+        except Exception as err:
+            err.data = msgDict
+            self.emit_failure(err)
             raise
         else:
             self._transport.async_send(
@@ -107,11 +125,20 @@ class gelfHandler(logging.Handler):
                 failure_cb=self.emit_failure)
 
     def emit_success(self):
+        '''
+        TODO
+        '''
         pass
 
-    def emit_failure(self, e, level=logging.ERROR):
+    def emit_failure(self, e, **_):
+        '''
+        TODO
+        '''
         print(repr(e))
 
     def close(self):
+        '''
+        TODO
+        '''
         if self.proto == 'TCP':
             self._transport.sock.close()
